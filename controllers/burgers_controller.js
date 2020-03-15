7151fd12abda101c1889a1937fff807a69317da0
Spencer Wood <spencer.wood@me.com>
2020-03-14 23:57:40 -0500

    created the burger module for the controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,5 @@
+var exporess = require('express');
+
+var router = exporess.Router();
+//import the model burger.js to use the database functions
+var burger = require('../models/burger.js');
---
+++ models/burger.js
@@ -0,0 +1,28 @@
+var orm = require('../config/orm.js');
+
+var burger = {
+    all: function(cb){
+        orm.all('burgers', function(res) {
+            cb(res);
+        });
+    },
+    //var cols and val are arrays
+    create: function(cols, vals, cb) {
+        orm.create('burgers', cols, vals, function(res){
+            cb(res);
+        });
+    },
+    update: function(objColVals, condition, cb) {
+        orm.update('burgers', objColVals, condition, function(res) {
+            cb(res);
+        });
+    },
+    delete: function(condition, cb) {
+        orm.delete('burgers', condition, function(res) {
+            cb(res);
+        });
+    }
+};
+
+//export the db function for the controller
+module.exports = burger;
