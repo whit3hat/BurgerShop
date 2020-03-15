04f11dbffda421f8d99d89b449b2339eb0bb2b90
Spencer Wood <spencer.wood@me.com>
2020-03-15 12:02:52 -0500

    Wrote the burgers_controllers.js file for routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,3 +3,47 @@ var exporess = require('express');
 var router = exporess.Router();
 //import the model burger.js to use the database functions
 var burger = require('../models/burger.js');
+
+//ROUTERS=========================================
+//baisc router
+router.get('/', function(req, res) {
+    burger.all(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render('index', hbsObject);
+    });
+});
+
+//api routers
+router.post('/api/burgers', function(req, res) {
+    burger.create([
+        'name', 'devour'
+    ], [
+        req.body.name, req.body.devour
+    ], function(result) {
+        //send back the id of the new route
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put('/api/burgers/:id', function(req, res) {
+    var status = 'id = ' + rq.params.id;
+
+    console.log('status', status);
+
+    burger.update({
+        devour: req.body.devour
+    }, condition, function(result) {
+        if (result.changedRows == 0) {
+
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+//export routes for the server.js to use
+module.exports = router;
