fd98d357afb564486c526d730bac74cdfe7d4930
Spencer Wood <spencer.wood@me.com>
2020-03-15 19:22:32 -0500

    deleted the delete function from model, updated the create function

--- models/burger.js
+++ models/burger.js
@@ -7,21 +7,19 @@ var burger = {
         });
     },
     //var cols and val are arrays
-    create: function(cols, vals, cb) {
-        orm.create('burgers', cols, vals, function(res){
-            cb(res);
-        });
+    create: function(name, cb) {
+        orm.create('burgers', [
+            'burger_name', 'devoured'
+        ], [
+            name, false
+        ], cb);
     },
+    
     update: function(objColVals, condition, cb) {
         orm.update('burgers', objColVals, condition, function(res) {
             cb(res);
         });
     },
-    delete: function(condition, cb) {
-        orm.delete('burgers', condition, function(res) {
-            cb(res);
-        });
-    }
 };
 
 //export the db function for the controller
