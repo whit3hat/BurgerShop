2d04385e86f9cb927d7a3c73b19681c035748d77
Spencer Wood <spencer.wood@me.com>
2020-03-22 12:48:04 -0500

    added JAWSDB to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,22 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'Madylin3!',
-    database: 'burgers_db'
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            passwor: 'Madylin3!',
+            database: 'burgers_db'
+    });
+}
+// var connection = mysql.createConnection({
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
+//     password: 'Madylin3!',
+//     database: 'burgers_db'
+// });
 
 //make connection to db
 connection.connect(function(err) {
